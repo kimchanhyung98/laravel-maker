fc53225a792e9374baef429fa564c4a43c4ec92b
kimchanhyung98 <kimchanhyung98@gmail.com>
2024-05-20 03:29:08 +0900

    Fix typo

--- config/maker.php
+++ config/maker.php
@@ -4,10 +4,10 @@
 return [
     'action' => [
         'namespace' => 'App\Actions',
-        'stub' => base_path('stubs/action.stub'),
+        'stub' => base_path('src/Stubs/action.stub'),
     ],
     'service' => [
         'namespace' => 'App\Services',
-        'stub' => base_path('stubs/service.stub'),
+        'stub' => base_path('src/Stubs/service.stub'),
     ],
 ];
